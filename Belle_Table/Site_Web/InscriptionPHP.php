8b9a56e55a48a885392e5dc3a93f6e7275f6f2a6
Nathan-Part <nathanpartouche2@gmail.com>
2020-10-16 15:09:42 +0200

    Restructuration
    General
    
    indentation general de toute les page au niveau du code pour que ce sois plus facile a lire
    
    	menu.php
    
    j'ai modifier la barre de menu j'ai tout mis dans un fichier "menu.php" et donc TOUTE les pages du site son relié a une seul et meme barre de menu ce qui permet plus de facilité pour modifier la barre sur toute les pages en meme temps etant donnée que toute les page son relié sur la meme j'ai du effectuer des modification sur le systeme de deconnexion
    avec le GET['info'] et l'endroit ou le session destroy et placé mais du coup le systeme est 100 operationel.
    
    remplacement se connecter/deconnecter par : mon compte -> mon profil si admin -> panel admin ; pour tous -> se deconnecter
    
    	index.php
    
    rajout d'un formulaire de contact en bas de page
    
    	footer.php
    
    pour le footer j'ai fait exactement pareil que pour le menu il y a un seul et meme footer pour toute les page relié a la page footer.php
    
    	inscription.php :
    
    input aligné dans la page inscription et repositionnée
    
    ajout de label pour relier texte + champs
    
    bouton submit modifié
    
    en rajoutant : 'or 1='1 en mdp on peu accedez au compte de n'importe quel utilisateur j'ai donc corrigé cette faille de securité !
    
    rajout de la possiblilité a l'utilisateur de pouvoir rajouter son prenom au lieu de son email pour se connecter
    
    background changé pour faire comme tout les autre page
    
    	infolegal.php
    
    restructuration de la largeur du texte de la page "info legal" toute les lignes s'etaler au max est n'etait pas bien aligné

--- Belle_Table/Site_Web/InscriptionPHP.php
+++ Belle_Table/Site_Web/InscriptionPHP.php
@@ -1,52 +1,53 @@
 <?php 
-session_start();
-if (isset($_POST["monboutinscription"])){
-$nom= $_POST["nom"];
-$prenom= $_POST["prenom"];
-$mdp= $_POST["mdp"];
-$email= $_POST["email"];
-$tel= $_POST["tel"];
-$id = mysqli_connect ("localhost", "root", "","ppe1"); // la ou il se connecte puis le user puis le mot de passe puis la BDD
-$req = "select * from login where email = '$email'";
-$res = mysqli_query($id,$req);
+	session_start();
+	
+	if (isset($_POST["monboutinscription"]))
+	{
+		$nom= $_POST["nom"];
+		$prenom= $_POST["prenom"];
+		$mdp= $_POST["mdp"];
+		$email= $_POST["email"];
+		$tel= $_POST["tel"];
+		$id = mysqli_connect ("localhost", "root", "","belle_table"); // la ou il se connecte puis le user puis le mot de passe puis la BDD
+		$req = "select * from login where email = '$email'";
+		$res = mysqli_query($id,$req);
 
-if (mysqli_num_rows($res) > 0)
-		{header("location:Inscription.php?erreur=1");}
+		if (mysqli_num_rows($res) > 0)
+			{
+				header("location:Inscription.php?erreur=1");
+			}
 		else
-		{
-			$req2 = "insert into login (nom, prenom, telephone, mdp, email, admin) values ('$nom','$prenom','$tel','$mdp', '$email', '0')";
-			$res = mysqli_query($id, $req2);
-			header("location:Inscription.php?inscrit=1");
-
-		}
-
-}
-
-
-
-
-
-
-if (isset($_POST["monboutconnexion"])){
-$mdp= $_POST["mdp"];
-$email= $_POST["email"];
-$id = mysqli_connect ("localhost", "root", "","ppe1"); // la ou il se connecte puis le user puis le mot de passe puis la BDD
-$req = "select * from login where email = '$email' and mdp = '$mdp'";
-$res = mysqli_query($id,$req);
-
-if (mysqli_num_rows($res) > 0){
-
-			$_SESSION["email"] = $email;
-			header("location:index.php");
-		
-		}
-		else
-		{
-			
-			header("location:Inscription.php?erreur1=1");
-
-		}
-
+			{
+				$req2 = "insert into login (nom, prenom, telephone, mdp, email, admin) values ('$nom','$prenom','$tel','$mdp', '$email', '0')";
+				$res = mysqli_query($id, $req2);
+				header("location:Inscription.php?inscrit=1");
+			}
 	}
 
- ?>
+	if (isset($_POST["monboutconnexion"]))
+	{
+		$mdp= str_replace("'","''",$_POST['mdp']);
+		$email= str_replace("'","''",$_POST['email']);
+		$id = mysqli_connect ("localhost", "root", "","belle_table"); // la ou il se connecte puis le user puis le mot de passe puis la BDD
+		$req = "SELECT * FROM login WHERE email = '{$email}' and mdp = '{$mdp}'";
+		$res = mysqli_query($id,$req);
+
+		if (mysqli_num_rows($res) > 0)
+			{	  
+				$ligne = mysqli_fetch_assoc($res);
+				$_SESSION["nom"] = $ligne['nom'];
+				$_SESSION["prenom"] = $ligne['prenom'];
+				$_SESSION["telephone"] = $ligne['telephone'];
+				$_SESSION["mdp"] = $ligne['mdp'];
+				$_SESSION["email"] = $ligne['email'];
+				$_SESSION["admin"] = $ligne['admin'];
+				header("location:index.php");
+			
+			}
+		else
+			{
+				header("location:Inscription.php?erreur1=1");
+			}
+	}
+
+?>
--- Belle_Table/Site_Web/offre.php
+++ Belle_Table/Site_Web/offre.php
@@ -1,132 +1,84 @@
 <!DOCTYPE html>
-	<html>
-	<?php session_start();?>
-		<head>
-			<meta charset="utf-8">
-			<link rel="stylesheet" type="text/css" href="styles.css">
-			<link href="https://fonts.googleapis.com/css?family=Crete+Round" rel="stylesheet">
-			<title>Belle Table</title>
-		</head>
-		<center>
-		<body>
-			<header>
-				<div class="wrapper Header">
-					<h1>Belle Table<span class="orange">.</span></h1>
-					<nav>
-						<ul>
-							<li><a href="index.php">Accueil</a></li>
-							<li><a href="info.php">Nous découvrir</a></li>
-							<li><a href="offre.php">Offres d'emplois</a></li>
-							<li><a href="produits.php">Nos produits</a></li>
-							<li><a href="infolegal.php">Infos legal</a></li>
-							<?php
-							if (!isset($_SESSION['email']))
-							{
-								?>
-								<li><a href="Inscription.php">Se connecter</a></li>
-								<?php
-							}
-							if (isset($_SESSION['email']))
-							{
-								?>
-								<li><a href="Inscription.php?infos=offre.php">Se deconnecter <?php } ?></a></li>
+<html>
+<head>
+	<?php session_start(); ?>
+	<?php $page = "offre.php"; ?>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Crete+Round" rel="stylesheet">
+	<title>Belle Table</title>
+</head>
+<body>
+	<center>
+		<?php include("menu.php"); ?>
 
-						</ul>
-					</nav>
-				</div>
-			</header>
-
-	<section id="possibilities">
-		<?php
-			if (!isset($_SESSION['email'])){
+		<section id="possibilities">
+			<?php
+			if (!isset($_SESSION['email']))
+			{
 				echo "connecte toi frere";
 			}
-			else{
-
+			else
+			{
 				$login = $_SESSION['email'];
-			if (!isset($_POST["boutlvl"]))
-			{	?>
-		<center>
-		<form method="post" action="offre.php">
-				<b> Choisir votre difficulté:</b> <br><br>Debutant<input type="radio" name="niveau" value="0"> <br>
-				Expert							<input type="radio" name="niveau" value="1" checked> <br> <br>
-				<input type="submit" value="Choisir" name="boutlvl">
-			</form>		
-			<?php   }   ?>
+				if (!isset($_POST["boutlvl"])) 
+				{ 	?>
+					<form method="post" action="offre.php">
+						<b> Choisir votre difficulté:</b> 
+						<br><br>
+						<label for="debutant">Debutant</label>
+						<input type="radio" name="niveau" id="debutant" value="0"><br>
+						<label for="expert">Expert</label>
+						<input type="radio" name="niveau" value="1" id="expert" checked><br><br>
+						<input type="submit" value="Choisir" name="boutlvl">
+					</form>		
+					<?php
+				} 	?>
 
+				<form action="resultat.php" method="post">
 
+					<?php if (isset($_POST["boutlvl"])) 
+					{ 
+						?>
+						<h1>QCM A COMPLETER</h1>
 
-<form action="resultat.php" method="post">
+						<?php
 
-<?php
-	if (isset($_POST["boutlvl"]))
-	{ ?>
+						$niveau = $_POST['niveau'];
+						$_SESSION['niveau'] = $niveau;
+						$id = mysqli_connect ("localhost", "root", "","belle_table");
+						mysqli_query($id,"SET NAMES 'utf8'");
+						$req = "select * from question where niveau = $niveau ORDER BY RAND() limit 10";
+						$res = mysqli_query($id, $req); 
 
+						while($ligne = mysqli_fetch_assoc($res))
+						{
+								echo '<br><br>';
+								echo '<b>'.$ligne["label"].":<b>"; 
+								//echo '<span class="terme">'.$data['Terme']."</span>:".$data['Definition'];
+							?>
+							 <br><br><br>
+							<?php
+									$id = mysqli_connect ("localhost", "root", "","belle_table");
+									$req2 = "select * from reponse where idq = ".$ligne["id"]." ORDER BY RAND()";
+									$res2 = mysqli_query($id, $req2);
 
-<h1>QCM A COMPLETER</h1>
-
-
-<?php
-$niveau = $_POST['niveau'];
-$_SESSION['niveau'] = $niveau;
-		$id = mysqli_connect ("localhost", "root", "","belle_table");
-		mysqli_query($id,"SET NAMES 'utf8'");
-		$req = "select * from question where niveau = $niveau ORDER BY RAND() limit 10";
-		$res = mysqli_query($id, $req); 
-while($ligne = mysqli_fetch_assoc($res)){
-echo '<br><br>';
-		echo '<b>'.$ligne["label"].":<b>"; 
-		//echo '<span class="terme">'.$data['Terme']."</span>:".$data['Definition'];
-?>
- <br><br><br>
-<?php
-		$id = mysqli_connect ("localhost", "root", "","belle_table");
-		$req2 = "select * from reponse where idq = ".$ligne["id"]." ORDER BY RAND()";
-		$res2 = mysqli_query($id, $req2);
-while($ligne2 = mysqli_fetch_assoc($res2)){ ?>
-
-<input type="radio" name="<?= $ligne2["idq"]; ?>" value="<?= $ligne2["verif"]; ?>" required> <?php echo $ligne2["reponse"]?> <br> <br>
-
-
-<?php }}?>
-<input type="submit" value="Envoyer"> <br><br><br>
-<?php } }  ?>
-
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			<section id="contact"> <!-- sert egalement de footer-->
-				<div class="wrapper">
-					<h3>Contactez-nous</h3>
-					<p>Adresse : 20, rue de la gare 75100 Paris.<br> N° de téléphone : 01 75 02 77 14. <br> Adresse Mail: azerty@gmail.com</p> 
-					<br><br>
-
-					<h1>Belle Table<span class="orange">.</span></h1>
-					<div class="copyright">Copyright © Tous droits réservés.</div>
-				</div>
-			</section>
-
+							while($ligne2 = mysqli_fetch_assoc($res2))
+							{ 
+								?>
+								<input type="radio" name="<?= $ligne2["idq"]; ?>" value="<?= $ligne2["verif"]; ?>" required> <?php echo $ligne2["reponse"]?> <br><br>
+								<?php 
+							} 	?>
+							<?php 
+						} 	?>
+						<input type="submit" value="Envoyer"> <br><br><br>
+						<?php 
+					} 	?>
+				</form>
+				<?php 
+			} 	?>
+		</section>
+		<?php include("footer.php"); ?>
+	</center>
 </body>
-</center>
 </html>
